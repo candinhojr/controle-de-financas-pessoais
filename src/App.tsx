e7938649227372d8a95edbef31d2e0c31e8370c4
Candinho Luiz Dalla Brida Junior <candinholuiz@gmail.com>
2022-01-14 16:46:44 -0300

    Criando e carregando as transações por meio da Context API

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import { Header } from "./components/Header";
 import { NewTransactionModal } from "./components/NewTransactionModal";
 
 import { GlobalStyle } from "./styles/global";
+import { TransactionsProvider } from "./TransactionsContext";
 
 Modal.setAppElement("#root");
 
@@ -13,14 +14,17 @@ export function App() {
   const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(
     false
   );
+
   function handleOpenNewTransactionModal() {
     setIsNewTransactionModalOpen(true);
   }
+
   function handleCloseNewTransactionModal() {
     setIsNewTransactionModalOpen(false);
   }
+
   return (
-    <>
+    <TransactionsProvider>
       <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
       <Dashboard />
       <NewTransactionModal
@@ -28,6 +32,6 @@ export function App() {
         onRequestClose={handleCloseNewTransactionModal}
       />
       <GlobalStyle />
-    </>
+    </TransactionsProvider>
   );
 }
